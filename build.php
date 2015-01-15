eb1920f089f8c0f091287ff22a4a9c7ce25fd5b9
Bert-Jan de Lange <info@bert-jan.com>
2015-01-15 15:40:37 +0100

    Upgraded to 3.1.0

--- build.php
+++ build.php
@@ -1,9 +1,24 @@
 #!/usr/bin/php
 <?php
-chdir(__DIR__ .'/vendor/hcatlin/sassc');
 
-// Compile sassc binary
-passthru('SASS_LIBSASS_PATH='. __DIR__ .'/vendor/hcatlin/libsass make');
+try {
+    if (false === chdir(__DIR__ . '/vendor/sass/sassc')) {
+        throw new Exception('error changing into '. __DIR__ . '/vendor/sass/sassc');
+    }
 
-// move it to bin dir
-rename('bin/sassc', __DIR__ .'/bin/sassc');
+    // Compile sassc binary
+    passthru('SASS_LIBSASS_PATH=' . __DIR__ . '/vendor/sass/libsass make', $return);
+
+    if (0 !== $return) {
+        throw new Exception('error compiling sassc');
+    }
+
+    // move it to bin dir
+    if (false === rename('bin/sassc', __DIR__ . '/bin/sassc')) {
+        throw new Exception('error moving bin/sassc to '. __DIR__ .'/bin/sassc');
+    }
+
+    echo 'Build complete !'. PHP_EOL;
+} catch (Exception $exception) {
+    echo 'Build failed: '. $exception->getMessage() . PHP_EOL;
+}
